0a409dc53c2b651bb44b33294d922e70eda1ce07
Peet Janse van Vuuren <peet@whereismytransport.com>
2017-05-18 18:13:12 +0200

    implemented: Update FareRule

--- GTFS.DB.SQLite/Collections/SQLiteFareRulesCollection.cs
+++ GTFS.DB.SQLite/Collections/SQLiteFareRulesCollection.cs
@@ -178,7 +178,28 @@ namespace GTFS.DB.SQLite.Collections
 
         public bool Update(string entityId, FareRule newEntity)
         {
-            throw new NotImplementedException();
+            string sql = "UPDATE fare_rule SET FEED_ID=:feed_id, fare_id=:fare_id, route_id=:route_id, origin_id=:origin_id, destination_id=:destination_id, contains_id=:contains_id WHERE fare_id=:entityId;";
+            using (var command = _connection.CreateCommand())
+            {
+                command.CommandText = sql;
+                command.Parameters.Add(new SQLiteParameter(@"feed_id", DbType.Int64));
+                command.Parameters.Add(new SQLiteParameter(@"fare_id", DbType.String));
+                command.Parameters.Add(new SQLiteParameter(@"route_id", DbType.String));
+                command.Parameters.Add(new SQLiteParameter(@"origin_id", DbType.String));
+                command.Parameters.Add(new SQLiteParameter(@"destination_id", DbType.String));
+                command.Parameters.Add(new SQLiteParameter(@"contains_id", DbType.String));
+                command.Parameters.Add(new SQLiteParameter(@"entityId", DbType.String));
+
+                command.Parameters[0].Value = _id;
+                command.Parameters[1].Value = newEntity.FareId;
+                command.Parameters[2].Value = newEntity.RouteId;
+                command.Parameters[3].Value = newEntity.OriginId;
+                command.Parameters[4].Value = newEntity.DestinationId;
+                command.Parameters[5].Value = newEntity.ContainsId;
+                command.Parameters[6].Value = entityId;
+
+                return command.ExecuteNonQuery() > 0;
+            }
         }
 
         public void AddRange(IUniqueEntityCollection<FareRule> entities)
