842fc7ac7c3a2504974ed2643068c5c90717a708
Tim Trewartha <timt@whereismytransport.com>
2017-06-19 16:17:12 +0200

    Seems to be connecting to local!

---
+++ GTFS.DB.PostgreSQL/PostgreSQLGTFSFeedDB.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Npgsql;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GTFS.DB.PostgreSQL
+{
+    public class PostgreSQLGTFSFeedDB : IGTFSFeedDB
+    {
+        /// <summary>
+        /// Creates a new db.
+        /// </summary>
+        public PostgreSQLGTFSFeedDB(string connectionString)
+        {
+            var connection = new NpgsqlConnection("Server=127.0.0.1;User Id=postgres;Password=x84yay;Database=GTFS");
+            connection.Open();
+        }
+
+        public int AddFeed()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int AddFeed(IGTFSFeed feed)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IGTFSFeed GetFeed(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<int> GetFeeds()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RemoveFeed(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
