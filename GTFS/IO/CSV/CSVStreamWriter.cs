b4dea45c446d18e22589f9e13f8aa8256c729f69
Tim Trewartha <timt@whereismytransport.com>
2017-06-23 10:50:58 +0200

    Changed all CSV reader/writer to user UTF8 with BOM
    - hopefully no further issues else will have to look at detecting encoding
      from file

--- GTFS/IO/CSV/CSVStreamWriter.cs
+++ GTFS/IO/CSV/CSVStreamWriter.cs
@@ -46,7 +46,7 @@ namespace GTFS.IO.CSV
         /// <param name="stream"></param>
         public CSVStreamWriter(Stream stream)
         {
-            _stream = new StreamWriter(stream, new System.Text.UTF8Encoding(false));
+            _stream = new StreamWriter(stream, new System.Text.UTF8Encoding(true));
         }
 
         /// <summary>
@@ -56,7 +56,7 @@ namespace GTFS.IO.CSV
         /// <param name="seperator"></param>
         public CSVStreamWriter(Stream stream, char seperator)
         {
-            _stream = new StreamWriter(stream, new System.Text.UTF8Encoding(false));
+            _stream = new StreamWriter(stream, new System.Text.UTF8Encoding(true));
             _seperator = seperator;
         }
 
