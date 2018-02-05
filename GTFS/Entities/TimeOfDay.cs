8676c8d00873e4f2b0f33af8264866df46fb8ab9
Peet Janse van Vuuren <peet@whereismytransport.com>
2018-02-05 14:16:21 +0200

    improved the logic

--- GTFS/Entities/TimeOfDay.cs
+++ GTFS/Entities/TimeOfDay.cs
@@ -94,9 +94,9 @@ namespace GTFS.Entities
                 {
                     if (tokens[i].Contains(" "))//adds a 0 to the end of the token if it only contains a single number
                     {
-                        tokens[i] = tokens[i].ToCharArray()[0] + "0";
+                        tokens[i] = tokens[i].Replace(' ', '0');
                     }
-                    else if (tokens[i] == "" || tokens[i] == "  ")//replaces all the chars in the token to 0's if it is empty
+                    else if (tokens[i] == "")//replaces all the chars in the token to 0's if it is empty
                     {
                         tokens[i] = "00";
                     }
